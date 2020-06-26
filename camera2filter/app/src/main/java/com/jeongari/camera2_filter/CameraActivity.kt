f6f7b76d5228b102bfe3d66b957ad478feaefa47
Jeongah-Shin <jeongah.h.shin@gmail.com>
2020-06-26 15:43:45 +0900

    Camera2 basis for camera view with filter(gpu-image)

--- camera2filter/app/src/main/java/com/jeongari/camera2_filter/CameraActivity.kt
+++ camera2filter/app/src/main/java/com/jeongari/camera2_filter/CameraActivity.kt
@@ -1,17 +1,20 @@
 package com.jeongari.camera2_filter
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import com.jeongari.camera2_filter.camera.Camera2BasicFragment
 
-class MainActivity : AppCompatActivity() {
+class CameraActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+        setContentView(R.layout.activity_camera)
+        if (null == savedInstanceState) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, Camera2BasicFragment.newInstance())
+                .commit()
+        }
     }
 
     /**
@@ -21,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     external fun stringFromJNI(): String
 
     companion object {
+
         // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
